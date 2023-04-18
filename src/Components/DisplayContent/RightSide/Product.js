c90d83e37b08e8d6fdc61d86ab2c63dea81b7dfd
Aakash <parateakash21.ap@gmail.com>
2023-04-18 13:05:41 +0530

    creating products

--- src/Components/DisplayContent/RightSide/Product.js
+++ src/Components/DisplayContent/RightSide/Product.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import "./RightSide.css";
+import Rating from '@mui/material/Rating';
 
 function Product(props) {
     return (
-        <div>
-            <div><img src=''/></div>
-            <div>
-                Name
+        <div className='product'>
+            <div className='Product_image'>
+                <img src={props.definition.image} style={{height : "300px"}} />
             </div>
-            <div>
-                Rating
+            <div className='Product_name'>
+                {props.definition.name}
             </div>
-            <div>
-                Price    
-            </div>           
+            <div className='Product_rating'>
+               {/* <span style={{color:"yellow", fontSize:"25px"}}>&#9733;&#9733;&#9733;&#9733;&#9734;</span>  for rating stars*/}
+               <Rating name="read-only" value={4} readOnly />
+                 <div>{props.definition.rating}</div> 
+            </div>
+            <div className='Product_price'>
+               <p style={{display: "flex"}}> {props.definition.price} <del style={{color : "grey", fontSize: "14px", padding: "6px"}}>&#8377;{props.definition.oprice}</del> <div style={{color : "black", fontSize: "14px", padding: "6px"}}>Save &#8377;{props.definition.save}</div></p>
+            </div>          
+            <div style={{marginTop: "-25px"}}>
+                Save extra with No Cost EMI 
+            </div> 
+            <div style={{color:"darkgreen", fontSize:"15px", marginTop:"2px"}}>
+                FREE Delivery by Amazon
+            </div>
         </div>
     ); 
 }
--- src/Components/DisplayContent/RightSide/RightSide.js
+++ src/Components/DisplayContent/RightSide/RightSide.js
@@ -1,11 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./RightSide.css";
 import Product from "./Product";
 
 function RightSide(props) {
+
+    const [listOfProduct,setListOfProducts] = useState([]);
+
+    useEffect(()=>{
+        //api call
+        let list =[
+            { name :  "New Apple iphone 11 (64GB) - Green", rating:"35,946", price:"50,900",image:"https://ik.imagekit.io/amazon26/amazon-image/mobiles/71w3oJ7aWyL._AC_UL640_FMwebp_QL65_.webp?updatedAt=1680608533501", oprice:"54,900", save:"3,901(7%)"},
+            { name :  "New Apple iphone 12 (128GB) - Blue", rating:"5,786", price:"79,000",image:"https://ik.imagekit.io/amazon26/amazon-image/mobiles/71ZOtNdaZCL._AC_UL640_FMwebp_QL65_.webp?updatedAt=1680608537496", oprice:"84,900", save:"5,900(7%)"},
+            { name :  "New Apple iphone 11 (64GB) - Black", rating:"35,786", price:"50,900",image:"https://ik.imagekit.io/amazon26/amazon-image/mobiles/71i2XhHU3pL._AC_UL640_FMwebp_QL65_.webp?updatedAt=1680608533256", oprice:"54,900", save:"3,901(7%)"},
+            { name :  "New Apple iphone XR (128GB) - White", rating:"124", price:"47,999",image:"https://ik.imagekit.io/amazon26/amazon-image/mobiles/51PuFBgBK4L._AC_UL640_FMwebp_QL65_.webp?updatedAt=1680608533107", oprice:"52,900", save:"4,901(9%)"}
+        
+        ]
+
+        setListOfProducts(list);
+
+    },[]);
+
+
     return(
         <div className="rightSide_main">
-            <Product/>
+            {/* <Product rating="35,786" price="50,999" name="New Apple iphone 11 (64GB) - Black" image="https://ik.imagekit.io/amazon26/amazon-image/mobiles/71i2XhHU3pL._AC_UL640_FMwebp_QL65_.webp?updatedAt=1680608533256" oprice="54,900" save="3,901(7%)"/>
+            <Product rating="" price="" name="" image="" oprice="" save=""/>
+            <Product rating="" price="" name="" image="" oprice="" save=""/>
+            <Product rating="" price="" name="" image="" oprice="" save=""/>
+            <Product rating="" price="" name="" image="" oprice="" save=""/>
+            <Product rating="" price="" name="" image="" oprice="" save=""/> */}
+
+            {
+                
+                listOfProduct.map ( (item) =>(
+                    <Product definition={item} />
+                ))
+            }
+
+
         </div>
     );
 }
