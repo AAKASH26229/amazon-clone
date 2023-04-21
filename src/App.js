981b584382135d1203681adf3ca119a0ca1a2621
Aakash <parateakash21.ap@gmail.com>
2023-04-21 12:35:10 +0530

    add cart

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import DisplayContent from './Components/DisplayContent/DisplayContent';
 import PlaceOrder from './Components/PlaceOrder/PlaceOrder';
 import Checkout from './Components/Checkout/Checkout';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import CartContextProvider from './Components/CartContext';
 
 function App() {
   return ( 
@@ -13,10 +14,9 @@ function App() {
       <div className="App">
         <Navbar />
 
+        <CartContextProvider >
         <Routes>  
          
-            
-
           <Route path="/display" element={<DisplayContent />} />
 
           <Route path="/order/:id" element={<PlaceOrder />} />
@@ -26,6 +26,7 @@ function App() {
           <Route path="" element={<MainPage />} />
             
         </Routes>
+        </CartContextProvider >
 
       </div>
     </Router>
--- src/Components/PlaceOrder/PlaceOrder.js
+++ src/Components/PlaceOrder/PlaceOrder.js
@@ -1,26 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import "./PlaceOrder.css";
 import Grid from "@mui/material/Grid";
 import Rating from '@mui/material/Rating';
 import Paper from '@mui/material/Paper';
 import { Link, useParams } from 'react-router-dom';
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
+import { CartContext } from '../CartContext';
 
 
 function PlaceOrder(props) {
-    
-    const [productDetails,setProductDetails] = useState([]);
+
+
+    const {item,size} = useContext(CartContext);
+    const [productDetails, setProductDetails] = useState([]);
     // for getting particular id we are using useParam
 
-    let  { id } = useParams(); 
+    let { id } = useParams();
 
-    useEffect(()=>{
-
+   
 
         //api call
         // let list =[
-                 
-        
+
+
         //             {
         //                 "id":2233,
         //                 "name":"",
@@ -126,118 +128,128 @@ function PlaceOrder(props) {
         //                             ]
         //                             }
         // ]
-        let list =[
-            
-                    { id: "1133" , name :  "New Apple iPhone 11 (64GB) - Green", rating:"35,946",review:"1000", price:"50,900",EMI:"2,401",
-                     delivery:"Wednesday, Aug 18",
-                     status:"In Stock",
-                     Soldby:"Darshita Electronics and Fullfilled by Amazon.",
-                     image:"https://ik.imagekit.io/amazon26/amazon-image/mobiles/71w3oJ7aWyL._AC_UL640_FMwebp_QL65_.webp?updatedAt=1680608533501", 
-                     oprice:"54,900", 
-                     save:"3,901(7%)", 
-                     prime:"Tomorrow, August 16", 
-                     Sizename: "64GB",
-                     Colour: "Green",
-                     ModelName: "iPhone 11",
-                     WirelessCarrier: "Unlocked for All Carriers",
-                     Brand: "Apple",
-                        },
-            
-                        { id: "1134", name :  "New Apple iPhone 12 (128GB) - Blue", rating:"5,786", price:"79,000",review:"1000",
-                         EMI:"2,401",
-                         delivery:"Wednesday, Aug 18",
-                         status:"In Stock",
-                         Soldby:"Darshita Electronics and Fullfilled by Amazon.",
-                         image:"https://ik.imagekit.io/amazon26/amazon-image/mobiles/71ZOtNdaZCL._AC_UL640_FMwebp_QL65_.webp?updatedAt=1680608537496", 
-                         oprice:"84,900", 
-                         save:"5,900(7%)", 
-                         prime:"Wednesday, August 18",
-                         Sizename: "128GB",
-                         Colour: "Blue",
-                         ModelName: "iPhone 12",
-                         WirelessCarrier: "Unlocked for All Carriers",
-                         Brand: "Apple"
-                        },
-            
-                         { id: "2233", name :  "New Apple iPhone 11 (64GB) - Black", rating:"35,786", price:"50,900",review:"1000",
-                             EMI:"2,401",
-                             delivery:"Wednesday, Aug 18",
-                             status:"In Stock",
-                             Soldby:"Darshita Electronics and Fullfilled by Amazon.",
-                            image:"https://ik.imagekit.io/amazon26/amazon-image/mobiles/71i2XhHU3pL._AC_UL640_FMwebp_QL65_.webp?updatedAt=1680608533256", 
-                            oprice:"54,900", 
-                            save:"3,901(7%)", 
-                            prime:"Wednesday, August 18",
-                            Sizename: "64GB",
-                            Colour: "Black",
-                            ModelName: "iPhone 11",
-                            WirelessCarrier: "Unlocked for All Carriers",
-                            Brand: "Apple"
-                         },
 
-                         { id: "2234", name :  "New Apple iPhone XR (128GB) - White", rating:"124", price:"47,999",review:"100",
-                                
-                                 EMI:"2,401",
-                                 delivery:"Wednesday, Aug 18",
-                                 status:"In Stock",
-                                 Soldby:"Darshita Electronics and Fullfilled by Amazon.",
-                                image:"https://ik.imagekit.io/amazon26/amazon-image/mobiles/51PuFBgBK4L._AC_UL640_FMwebp_QL65_.webp?updatedAt=1680608533107", 
-                                oprice:"52,900", 
-                                save:"4,901(9%)", 
-                                prime:"Tuesday, August 17",
-                                Sizename: "128GB",
-                                 Colour: "White",
-                                 ModelName: "iPhone XR",
-                                 WirelessCarrier: "Unlocked for All Carriers",
-                                 Brand: "Apple"
-                                },
+        useEffect(() => {
 
-                                { id: "3344", name :  "New Apple iPhone 11 (64GB) - Black", rating:"35,786", price:"50,900",review:"1000",
-                            
-                                EMI:"2,401",
-                                delivery:"Wednesday, Aug 18",
-                                status:"In Stock",
-                                Soldby:"Darshita Electronics and Fullfilled by Amazon.",
-                               image:"https://ik.imagekit.io/amazon26/amazon-image/mobiles/71i2XhHU3pL._AC_UL640_FMwebp_QL65_.webp?updatedAt=1680608533256", 
-                               oprice:"54,900", 
-                               save:"3,901(7%)", 
-                               prime:"Wednesday, August 18",
-                               Sizename: "64GB",
-                               Colour: "Black",
-                               ModelName: "iPhone 11",
-                               WirelessCarrier: "Unlocked for All Carriers",
-                               Brand: "Apple"
-                            },
-   
-                            { id: "3345", name :  "New Apple iPhone XR (128GB) - White", rating:"124", price:"47,999",review:"100",
-                                    EMI:"2,401",
-                                    delivery:"Wednesday, Aug 18",
-                                    status:"In Stock",
-                                    Soldby:"Darshita Electronics and Fullfilled by Amazon.",
-                                   image:"https://ik.imagekit.io/amazon26/amazon-image/mobiles/51PuFBgBK4L._AC_UL640_FMwebp_QL65_.webp?updatedAt=1680608533107", 
-                                   oprice:"52,900", 
-                                   save:"4,901(9%)", 
-                                   prime:"Tuesday, August 17",
-                                   Sizename: "128GB",
-                                    Colour: "White",
-                                    ModelName: "iPhone XR",
-                                    WirelessCarrier: "Unlocked for All Carriers",
-                                    Brand: "Apple"
-                                   }
+
+
+        let list = [
+
+            {
+                id: "1133", name: "New Apple iPhone 11 (64GB) - Green", rating: "35,946", review: "1000", price: "50,900", EMI: "2,401",
+                delivery: "Wednesday, Aug 18",
+                status: "In Stock",
+                Soldby: "Darshita Electronics and Fullfilled by Amazon.",
+                image: "https://ik.imagekit.io/amazon26/amazon-image/mobiles/71w3oJ7aWyL._AC_UL640_FMwebp_QL65_.webp?updatedAt=1680608533501",
+                oprice: "54,900",
+                save: "3,901(7%)",
+                prime: "Tomorrow, August 16",
+                Sizename: "64GB",
+                Colour: "Green",
+                ModelName: "iPhone 11",
+                WirelessCarrier: "Unlocked for All Carriers",
+                Brand: "Apple",
+            },
+
+            {
+                id: "1134", name: "New Apple iPhone 12 (128GB) - Blue", rating: "5,786", price: "79,000", review: "1000",
+                EMI: "2,401",
+                delivery: "Wednesday, Aug 18",
+                status: "In Stock",
+                Soldby: "Darshita Electronics and Fullfilled by Amazon.",
+                image: "https://ik.imagekit.io/amazon26/amazon-image/mobiles/71ZOtNdaZCL._AC_UL640_FMwebp_QL65_.webp?updatedAt=1680608537496",
+                oprice: "84,900",
+                save: "5,900(7%)",
+                prime: "Wednesday, August 18",
+                Sizename: "128GB",
+                Colour: "Blue",
+                ModelName: "iPhone 12",
+                WirelessCarrier: "Unlocked for All Carriers",
+                Brand: "Apple"
+            },
+
+            {
+                id: "2233", name: "New Apple iPhone 11 (64GB) - Black", rating: "35,786", price: "50,900", review: "1000",
+                EMI: "2,401",
+                delivery: "Wednesday, Aug 18",
+                status: "In Stock",
+                Soldby: "Darshita Electronics and Fullfilled by Amazon.",
+                image: "https://ik.imagekit.io/amazon26/amazon-image/mobiles/71i2XhHU3pL._AC_UL640_FMwebp_QL65_.webp?updatedAt=1680608533256",
+                oprice: "54,900",
+                save: "3,901(7%)",
+                prime: "Wednesday, August 18",
+                Sizename: "64GB",
+                Colour: "Black",
+                ModelName: "iPhone 11",
+                WirelessCarrier: "Unlocked for All Carriers",
+                Brand: "Apple"
+            },
+
+            {
+                id: "2234", name: "New Apple iPhone XR (128GB) - White", rating: "124", price: "47,999", review: "100",
+
+                EMI: "2,401",
+                delivery: "Wednesday, Aug 18",
+                status: "In Stock",
+                Soldby: "Darshita Electronics and Fullfilled by Amazon.",
+                image: "https://ik.imagekit.io/amazon26/amazon-image/mobiles/51PuFBgBK4L._AC_UL640_FMwebp_QL65_.webp?updatedAt=1680608533107",
+                oprice: "52,900",
+                save: "4,901(9%)",
+                prime: "Tuesday, August 17",
+                Sizename: "128GB",
+                Colour: "White",
+                ModelName: "iPhone XR",
+                WirelessCarrier: "Unlocked for All Carriers",
+                Brand: "Apple"
+            },
+
+            {
+                id: "3344", name: "New Apple iPhone 11 (64GB) - Black", rating: "35,786", price: "50,900", review: "1000",
+                EMI: "2,401",
+                delivery: "Wednesday, Aug 18",
+                status: "In Stock",
+                Soldby: "Darshita Electronics and Fullfilled by Amazon.",
+                image: "https://ik.imagekit.io/amazon26/amazon-image/mobiles/71i2XhHU3pL._AC_UL640_FMwebp_QL65_.webp?updatedAt=1680608533256",
+                oprice: "54,900",
+                save: "3,901(7%)",
+                prime: "Wednesday, August 18",
+                Sizename: "64GB",
+                Colour: "Black",
+                ModelName: "iPhone 11",
+                WirelessCarrier: "Unlocked for All Carriers",
+                Brand: "Apple"
+            },
+
+            {
+                id: "3345", name: "New Apple iPhone XR (128GB) - White", rating: "124", price: "47,999", review: "100",
+                EMI: "2,401",
+                delivery: "Wednesday, Aug 18",
+                status: "In Stock",
+                Soldby: "Darshita Electronics and Fullfilled by Amazon.",
+                image: "https://ik.imagekit.io/amazon26/amazon-image/mobiles/51PuFBgBK4L._AC_UL640_FMwebp_QL65_.webp?updatedAt=1680608533107",
+                oprice: "52,900",
+                save: "4,901(9%)",
+                prime: "Tuesday, August 17",
+                Sizename: "128GB",
+                Colour: "White",
+                ModelName: "iPhone XR",
+                WirelessCarrier: "Unlocked for All Carriers",
+                Brand: "Apple"
+            }
         ]
         //fake API
-        let item = list.filter( item => {
-            if(item.id === (id)) 
+        let item = list.filter(item => {
+            if (item.id === (id))
                 return item;
         })
-        
+
         console.log(item);
         setProductDetails(item[0]);
 
-    },[]);
+    }, []);
 
-    
-    return(
+
+    return (
         <div>
             <Grid container>
                 <Grid item xs={5}>
@@ -245,32 +257,32 @@ function PlaceOrder(props) {
                 </Grid>
                 <Grid item xs={4}>
                     <div className='placeorder_description'>
-                        <div style={{fontSize:"24px", lineHeight:"32px", fontWeight:500}}> {productDetails?.name} </div>
+                        <div style={{ fontSize: "24px", lineHeight: "32px", fontWeight: 500 }}> {productDetails?.name} </div>
                         <div>
-                            <Rating name="read-only" value="4" readOnly style={{fontSize:"20px"}}/>
+                            <Rating name="read-only" value="4" readOnly style={{ fontSize: "20px" }} />
                             {productDetails?.rating} | {productDetails?.review}+ answered questions
                         </div>
                         <hr></hr>
                         <div>
-                            <div className='textgap'><span className='textcolor'>M.R.P:</span> <del style={{color : "grey"}}>&#8377;{productDetails?.oprice}</del></div>
+                            <div className='textgap'><span className='textcolor'>M.R.P:</span> <del style={{ color: "grey" }}>&#8377;{productDetails?.oprice}</del></div>
                             <div className='textgap'><span className='textcolor'>Price:</span><span className='pricetag'> &#8377;{productDetails?.price}</span></div>
                             <div className='textgap'><span className='textcolor'>You Save:</span> &#8377;{productDetails?.save}</div>
                             <div className='textgap'>Inclusive of all taxes</div>
-                            <div className='textgap'><span style={{color:"blue"}}>FREE delivery:</span><strong> {productDetails?.delivery}</strong></div>
+                            <div className='textgap'><span style={{ color: "blue" }}>FREE delivery:</span><strong> {productDetails?.delivery}</strong></div>
                             <div className='textgap'>EMI Starts at &#8377;{productDetails?.EMI}. No Cost EMI available</div>
-                            <div className='textgap' style={{color:"green",fontSize:"20px",fontWeight:"500"}}>{productDetails?.status}.</div>
-                            <div className='textgap'>Sold by <span style={{color:"blue"}}>{productDetails?.Soldby}</span></div>
-                            <div className='textgap' style={{color:"blue"}}>10-day replacement only</div>
+                            <div className='textgap' style={{ color: "green", fontSize: "20px", fontWeight: "500" }}>{productDetails?.status}.</div>
+                            <div className='textgap'>Sold by <span style={{ color: "blue" }}>{productDetails?.Soldby}</span></div>
+                            <div className='textgap' style={{ color: "blue" }}>10-day replacement only</div>
                         </div>
-                        <div>    
+                        <div>
                             <br></br>
-                            <div style={{fontSize:"24px"}} className='textgap'>About this item</div>
+                            <div style={{ fontSize: "24px" }} className='textgap'>About this item</div>
                             <div>
                                 <div className='textgap'><span className='textcolor'>Size name:</span> <strong>{productDetails?.Sizename}</strong></div>
                                 <div className='textgap'><span className='textcolor'>Colour:</span> <strong>{productDetails?.Colour}</strong> </div>
                                 <div className='textgap'><span className='textcolor'>Pattern name:</span> <strong>{productDetails?.ModelName}</strong></div>
-                                <div className='textgap'><span style={{fontWeight:"bold"}}>Wireless Carrier: </span>{productDetails?.WirelessCarrier}</div>
-                                <div className='textgap'><span style={{fontWeight:"bold"}}>Brand: </span>{productDetails?.Brand}</div>
+                                <div className='textgap'><span style={{ fontWeight: "bold" }}>Wireless Carrier: </span>{productDetails?.WirelessCarrier}</div>
+                                <div className='textgap'><span style={{ fontWeight: "bold" }}>Brand: </span>{productDetails?.Brand}</div>
                                 <ul>
                                     <li>6.1-inch (15.5 cm diagonal) Liquid Retina HD LCD display</li>
                                     <li>Water and dust resistant (2 meters for up to 30 minutes, IP68)</li>
@@ -286,18 +298,18 @@ function PlaceOrder(props) {
                     <Paper variant='outlined' className='placeorder_order'>
                         <div>
                             <div>
-                                <div style={{fontWeight:"bold", marginTop:"20px", marginLeft:"30px"}}>Without Exchange</div>
-                                <div style={{ marginLeft:"30px"}}>&#8377;{productDetails.price}</div>
-                                <div style={{fontWeight:"bold",  marginTop:"20px", marginLeft:"30px"}}>Add an Accessory</div>
-                                <div style={{ marginLeft:"30px"}}><input type='checkbox' />Apple Airpods</div>
-                                <div style={{ marginLeft:"30px"}}><input type='checkbox' />Apple 20W USB Power Adapter</div>
+                                <div style={{ fontWeight: "bold", marginTop: "20px", marginLeft: "30px" }}>Without Exchange</div>
+                                <div style={{ marginLeft: "30px" }}>&#8377;{productDetails.price}</div>
+                                <div style={{ fontWeight: "bold", marginTop: "20px", marginLeft: "30px" }}>Add an Accessory</div>
+                                <div style={{ marginLeft: "30px" }}><input type='checkbox' />Apple Airpods</div>
+                                <div style={{ marginLeft: "30px" }}><input type='checkbox' />Apple 20W USB Power Adapter</div>
                             </div>
                             <div>
                                 <Link to={"/checkout"}>
-                                <button style={{marginTop:"20px", marginLeft:"20px"}} className='placeorder_button addtocart'>Add to Cart</button>
+                                    <button style={{ marginTop: "20px", marginLeft: "20px" }} className='placeorder_button addtocart'>Add to Cart</button>
                                 </Link>
-                                <button style={{marginTop:"10px", marginLeft:"20px"}} className='placeorder_button buynow'>Buy Now</button>
-                            </div>    
+                                <button style={{ marginTop: "10px", marginLeft: "20px" }} className='placeorder_button buynow'>Buy Now</button>
+                            </div>
                         </div>
                     </Paper>
                 </Grid>
